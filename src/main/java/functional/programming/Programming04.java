62b0f712949d5421dbb1a9d9a49ab00ea3f27011
Yogesh Deshmukh <sampoornesh@Yogeshs-MacBook-Air.local>
2020-04-25 20:34:45 +0530

    Added Custom Comparator

--- src/main/java/functional/programming/Programming04.java
+++ src/main/java/functional/programming/Programming04.java
@@ -2,6 +2,7 @@ package functional.programming;/*
 @Author : Yogesh Deshmukh
 */
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Programming04 {
@@ -9,14 +10,36 @@ public class Programming04 {
     public static void main(String[] args) {
 
         System.out.println("Distinct Number in the Array are : ");
-        printDistinctElements(List.of(12,4,5,3,12,99,44,3,66,66,77,88));
+        printDistinctElements(List.of(12,4,5,3,12,99,44,3,111,66,77,88));
 
         System.out.println("Sorted Number in the Array are : ");
-        printSortedElements(List.of(12,4,5,3,12,99,44,3,66,66,77,88));
+        printSortedElements(List.of(12,4,5,3,12,99,44,3,111,66,77,88));
 
         System.out.println("Sorted Distinct Number in the Array are : ");
-        printSortedDistinctElements(List.of(12,4,5,3,12,99,44,3,66,66,77,88));
-        
+        printSortedDistinctElements(List.of(12,4,5,3,12,99,44,3,111,66,77,88));
+
+        System.out.println("Sorted Distinct Number using comparator in the Array are : ");
+        printSortedDistinctElementsUsingComparator(List.of(12,4,5,3,12,99,44,3,111,66,77,88));
+
+        System.out.println("Reverse Sorted Number using comparator in the Array are : ");
+        printSortedReverseElementsUsingComparator(List.of(12,4,5,3,12,99,44,3,111,66,77,88));
+
+        System.out.println("Custom Sorted Number using comparator in the Array are : ");
+        printCustomSortedElementsUsingComparator(List.of("Spring", "Spring Boot", "Core Java", "Java","MicroServices", "Docker", "Kubernetos","PCF", "AWS", "Azure"));
+
+
+    }
+
+    private static void printCustomSortedElementsUsingComparator(List<String> courses) {
+        courses.stream()
+                .sorted(Comparator.comparing(str -> str.length()))
+                .forEach(System.out::println);
+    }
+
+    private static void printSortedReverseElementsUsingComparator(List<Integer> numbers) {
+        numbers.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
     }
 
     private static void printDistinctElements(List<Integer> numbers){
@@ -35,6 +58,13 @@ public class Programming04 {
         numbers.stream()
                 .distinct()
                 .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void printSortedDistinctElementsUsingComparator(List<Integer> numbers){
+        numbers.stream()
+                .distinct()
+                .sorted(Comparator.naturalOrder())
                 .forEach(System.out::println);
     }
 }
